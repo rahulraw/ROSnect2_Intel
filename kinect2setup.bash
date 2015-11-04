d06b15edeecb8f7416563189d09953e80e9c2725
rahulraw <rahulrawat.can@gmail.com>
2015-11-04 10:10:26 -0500

    script will now modify hosts/hostname files

--- kinect2setup.bash
+++ kinect2setup.bash
@@ -54,5 +54,9 @@ cd catkin_ws/src/iai_kinect2/
 rosrun robot_upstart install --master http://c1:11311 kinect2_bridge/launch/
 sudo sed -i '/^exit 0/isudo chmod 666 /dev/dri/*' /etc/rc.local
 
+# Replace hostname and hosts file 
+sudo cp -i ~/ROSnect2_Intel/hosts /etc/hosts
+sudo cp -i ~/ROSnect2_Intel/hostname /etc/hostname
+
 # Source bashrc final time
 . ~/.bashrc
